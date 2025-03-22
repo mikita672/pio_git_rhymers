c600594e1f225d075ad45e038dd5c2251f9f19d6
lostbtw <bozo@>
2025-03-22 12:41:56 +0100

    6. Deklaracja stalych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    private static final int TOTAL_NUMBERS = 12;
+    private static final int TOTAL_START = -1;
+    private static final int ERROR_CODE_DEFAULT = -1;
+    private int[] numbers = new int[TOTAL_NUMBERS];
 
-    public int total = -1;
+    public int total = TOTAL_NUMBERS;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == TOTAL_START;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == TOTAL_NUMBERS + TOTAL_START;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
-        return numbersgit[total];
+            return ERROR_CODE_DEFAULT;
+        return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERROR_CODE_DEFAULT;
         return numbers[total--];
     }
 
