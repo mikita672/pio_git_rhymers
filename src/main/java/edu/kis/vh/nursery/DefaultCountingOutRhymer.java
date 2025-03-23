087001a45a5ce71b4f0a90f7583c5049d1b63abc
Mikita Dzeviatau <81618202+mikita672@users.noreply.github.com>
2025-03-23 15:03:55 +0100

    10.Wygenerowano getter dla pola total w klasie DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,6 +7,10 @@ public class DefaultCountingOutRhymer {
     private static final int ERROR_CODE_DEFAULT = -1;
     private final int[] numbers = new int[TOTAL_NUMBERS];
 
+    public int getTotal() {
+        return total;
+    }
+
     private int total = TOTAL_START;
 
     protected void countIn(int in) {
